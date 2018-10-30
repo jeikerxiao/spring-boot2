6357f42b68f8095f86093bc9eb070c0093992616
jeikerxiao <jeiker@126.com>
2018-10-30 10:01:27 +0800

    init dubbo-server

---
+++ spring-boot-dubbo/dubbo-server/src/main/java/com/jeiker/dubboserver/DubboServerApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.dubboserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboServerApplication.class, args);
+    }
+}
