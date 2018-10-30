af9de1aba86b4104141d4d6b2c7b1658ebf17d84
jeikerxiao <jeiker@126.com>
2018-10-30 18:40:47 +0800

    init hello world

---
+++ spring-boot-docker/src/main/java/com/jeiker/docker/SpringBootDockerApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+}
---
+++ spring-boot-docker/src/main/java/com/jeiker/docker/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.jeiker.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Description: spring-boot-docker
+ * User: jeikerxiao
+ * Date: 2018/10/30 下午6:37
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public Map<String, String> hello() {
+        return Collections.singletonMap("message", "Hello World!");
+    }
+}
