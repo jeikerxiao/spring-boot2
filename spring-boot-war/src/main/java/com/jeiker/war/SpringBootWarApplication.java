013a6f8f2b355d84006871e66ef45b33eca072bf
jeikerxiao <jeiker@126.com>
2018-11-01 10:04:33 +0800

    package to war

--- spring-boot-war/src/main/java/com/jeiker/war/SpringBootWarApplication.java
+++ spring-boot-war/src/main/java/com/jeiker/war/SpringBootWarApplication.java
@@ -2,11 +2,18 @@ package com.jeiker.war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWarApplication {
+public class SpringBootWarApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWarApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
--- spring-boot-war/src/main/java/com/jeiker/war/controller/HelloController.java
+++ spring-boot-war/src/main/java/com/jeiker/war/controller/HelloController.java
@@ -1,9 +1,21 @@
 package com.jeiker.war.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
 /**
  * Description: spring-boot-war
  * User: jeikerxiao
  * Date: 2018/11/1 上午9:52
  */
+@RestController
 public class HelloController {
+
+    @RequestMapping("")
+    public Map<String, String> hi() {
+        return Collections.singletonMap("message", "This Spring Boot War");
+    }
 }
