98b8238fce0e1983411af6150fbdfe3562aa6acc
jeikerxiao <jeiker@126.com>
2018-11-01 09:53:53 +0800

    init war project

---
+++ spring-boot-war/src/main/java/com/jeiker/war/SpringBootWarApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWarApplication.class, args);
+    }
+}
---
+++ spring-boot-war/src/main/java/com/jeiker/war/controller/HelloController.java
@@ -0,0 +1,9 @@
+package com.jeiker.war.controller;
+
+/**
+ * Description: spring-boot-war
+ * User: jeikerxiao
+ * Date: 2018/11/1 上午9:52
+ */
+public class HelloController {
+}
