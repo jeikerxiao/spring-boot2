43daad0693254b7121d3ef0a64929fc6336c273a
jeikerxiao <jeiker@126.com>
2018-12-11 09:11:19 +0800

    add spring boot rabbitmq

--- spring-boot-rabbitmq/src/test/java/com/jeiker/rabbitmq/RabbitmqApplicationTests.java
+++ spring-boot-rabbitmq/src/test/java/com/jeiker/rabbitmq/RabbitmqApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jeiker.jpa;
+package com.jeiker.rabbitmq;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JpaApplicationTests {
+public class RabbitmqApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
