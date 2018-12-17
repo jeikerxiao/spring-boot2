1803579a9844237d741cfbaf09fe3f1fd0179757
jeikerxiao <jeiker@126.com>
2018-12-17 16:35:21 +0800

    add spring-boot-logback project

---
+++ spring-boot-logback/src/main/java/com/jeiker/logback/SpringBootLogbackApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLogbackApplication.class, args);
+    }
+
+}
+
