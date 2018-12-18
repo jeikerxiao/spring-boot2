3d9bb8f583af6081c04aa9858ed4c49ae4666bbd
jeikerxiao <jeiker@126.com>
2018-12-18 11:08:42 +0800

    add spring-boot-mybatis
    add spring-boot-mybatis

---
+++ spring-boot-mybatis/src/main/java/com/jeiker/mybatis/SpringBootMybatisApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisApplication.class, args);
+	}
+
+}
+
