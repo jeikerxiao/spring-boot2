ab093b391eed38ba6e2ada27dcdc8f80e80df4de
jeikerxiao <jeiker@126.com>
2018-12-18 14:59:52 +0800

    add spring-boot-camel
    add spring-boot-camel

---
+++ spring-boot-camel/src/main/java/com/jeiker/camel/SpringBootCamelApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCamelApplication.class, args);
+	}
+
+}
+
