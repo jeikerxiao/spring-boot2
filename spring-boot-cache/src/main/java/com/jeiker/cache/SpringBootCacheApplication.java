bb5e04a05d6771d331a8c745accebdaac2059e76
jeikerxiao <jeiker@126.com>
2018-12-18 10:30:19 +0800

    add spring-boot-cache
    add spring-boot-cache

---
+++ spring-boot-cache/src/main/java/com/jeiker/cache/SpringBootCacheApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCacheApplication.class, args);
+	}
+
+}
+
