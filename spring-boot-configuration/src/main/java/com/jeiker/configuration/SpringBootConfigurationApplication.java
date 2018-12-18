26011a43d0133c07738d89b542f530e5db9526bc
jeikerxiao <jeiker@126.com>
2018-12-18 10:26:00 +0800

    add spring-boot-configuration
    add spring-boot-configuration

---
+++ spring-boot-configuration/src/main/java/com/jeiker/configuration/SpringBootConfigurationApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigurationApplication.class, args);
+	}
+
+}
+
