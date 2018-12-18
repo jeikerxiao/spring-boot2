16eb2d8ca810fdeaa23b8ab3f3d3be884de88cb8
jeikerxiao <jeiker@126.com>
2018-12-18 14:55:22 +0800

    add spring-boot-quartz
    add spring-boot-quartz

---
+++ spring-boot-batch/src/main/java/com/jeiker/batch/SpringBootBatchApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchApplication.class, args);
+	}
+
+}
+
