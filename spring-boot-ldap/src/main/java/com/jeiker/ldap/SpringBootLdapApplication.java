6632f93a31025406d9f56b05d5d8bc5c82a0d493
jeikerxiao <jeiker@126.com>
2018-12-18 15:35:39 +0800

    add spring-boot-ldap
    add spring-boot-ldap

---
+++ spring-boot-ldap/src/main/java/com/jeiker/ldap/SpringBootLdapApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLdapApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot-ldap/src/test/java/com/jeiker/ldap/SpringBootLdapApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jeiker.ldap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLdapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
