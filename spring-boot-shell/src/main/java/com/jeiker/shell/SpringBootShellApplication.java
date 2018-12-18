93c1376a8c2b1d2b3d915f3e0813c02546888c5c
jeikerxiao <jeiker@126.com>
2018-12-18 15:01:58 +0800

    add spring-boot-shell
    add spring-boot-shell

---
+++ spring-boot-shell/src/main/java/com/jeiker/shell/SpringBootShellApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.shell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShellApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot-shell/src/test/java/com/jeiker/shell/SpringBootShellApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jeiker.shell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootShellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
