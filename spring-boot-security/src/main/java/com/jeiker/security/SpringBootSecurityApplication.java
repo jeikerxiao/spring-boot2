5db91418ab29dc097f6b8d8d177da4b2fb6280b9
jeikerxiao <jeiker@126.com>
2018-12-18 10:20:21 +0800

    add spring-boot-security
    add spring-boot-security

---
+++ spring-boot-security/src/main/java/com/jeiker/security/SpringBootSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot-security/src/test/java/com/jeiker/security/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jeiker.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
