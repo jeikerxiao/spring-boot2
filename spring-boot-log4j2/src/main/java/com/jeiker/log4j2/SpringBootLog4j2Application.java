5682222e066eb3e405019f1099b7222d165cbcd8
jeikerxiao <jeiker@126.com>
2018-12-18 09:42:48 +0800

    add spring-boot-log4j2
    add spring-boot-log4j2

---
+++ spring-boot-log4j2/src/main/java/com/jeiker/log4j2/SpringBootLog4j2Application.java
@@ -0,0 +1,14 @@
+package com.jeiker.log4j2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLog4j2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLog4j2Application.class, args);
+	}
+
+}
+
---
+++ spring-boot-log4j2/src/test/java/com/jeiker/log4j2/SpringBootLog4j2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jeiker.log4j2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLog4j2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
