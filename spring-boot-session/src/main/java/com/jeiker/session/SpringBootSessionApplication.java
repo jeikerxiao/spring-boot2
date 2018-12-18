61808d40fe3b1f5a1272f4e6e89d43e70b65c2fe
jeikerxiao <jeiker@126.com>
2018-12-18 10:28:06 +0800

    add spring-boot-session
    add spring-boot-session

---
+++ spring-boot-session/src/main/java/com/jeiker/session/SpringBootSessionApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionApplication.class, args);
+	}
+
+}
+
