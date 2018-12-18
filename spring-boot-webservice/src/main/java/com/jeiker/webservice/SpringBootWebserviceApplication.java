762710b1d1e70eca3a768572b0f5e3e88b6db697
jeikerxiao <jeiker@126.com>
2018-12-18 10:51:28 +0800

    add spring-boot-webservice
    add spring-boot-webservice

---
+++ spring-boot-webservice/src/main/java/com/jeiker/webservice/SpringBootWebserviceApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebserviceApplication.class, args);
+	}
+
+}
+
