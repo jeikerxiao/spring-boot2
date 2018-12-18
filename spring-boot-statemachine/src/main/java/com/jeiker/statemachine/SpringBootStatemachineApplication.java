c184556f9ca06cbf144a8e26560a2a99ec6ed4df
jeikerxiao <jeiker@126.com>
2018-12-18 15:15:27 +0800

    add spring-boot-statemachine
    add spring-boot-statemachine

---
+++ spring-boot-statemachine/src/main/java/com/jeiker/statemachine/SpringBootStatemachineApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.statemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStatemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStatemachineApplication.class, args);
+	}
+
+}
+
