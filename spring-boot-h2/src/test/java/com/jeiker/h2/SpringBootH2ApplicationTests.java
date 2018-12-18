c040db4f0351887e95d23723abd163f95ba9e813
jeikerxiao <jeiker@126.com>
2018-12-18 11:05:46 +0800

    add spring-boot-h2
    add spring-boot-h2

---
+++ spring-boot-h2/src/test/java/com/jeiker/h2/SpringBootH2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jeiker.h2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootH2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
