65ce3566bcd0eb77986f90b2142bf3e739655355
jeikerxiao <jeiker@126.com>
2018-12-18 14:16:19 +0800

    add spring-boot-neo4j
    add spring-boot-neo4j

---
+++ spring-boot-neo4j/src/main/java/com/jeiker/neo4j/SpringBootNeo4jApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNeo4jApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot-neo4j/src/test/java/com/jeiker/neo4j/SpringBootNeo4jApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jeiker.neo4j;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootNeo4jApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
