e50a0473a5425c242c4bb91d6b1c342cd8e1b43d
jeikerxiao <jeiker@126.com>
2018-12-18 09:40:29 +0800

    add spring-boot-log4j
    add spring-boot-log4j

---
+++ spring-boot-log4j/src/main/java/com/jeiker/log4j/SpringBootLog4jApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLog4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLog4jApplication.class, args);
+	}
+
+}
+
