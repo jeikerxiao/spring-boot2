e8851beac646825e37018b6329843dfb8d9b504b
jeikerxiao <jeiker@126.com>
2018-12-18 10:24:08 +0800

    add spring-boot-lombok
    add spring-boot-lombok

---
+++ spring-boot-lombok/src/main/java/com/jeiker/lombok/SpringBootLombokApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLombokApplication.class, args);
+	}
+
+}
+
