94d51dc676e6c39a4a6b19c8c397440a3235497f
jeikerxiao <jeiker@126.com>
2018-12-18 10:33:52 +0800

    add spring-boot-validation
    add spring-boot-validation

---
+++ spring-boot-validation/src/main/java/com/jeiker/validation/SpringBootValidationApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootValidationApplication.class, args);
+	}
+
+}
+
