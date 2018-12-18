e7ee1d9f06e5c5f54bc4d2a725c05f184f8fbab6
jeikerxiao <jeiker@126.com>
2018-12-18 14:03:02 +0800

    add spring-boot-redis
    add spring-boot-redis

---
+++ spring-boot-liquibase/src/main/java/com/jeiker/liquibase/SpringBootLiquibaseApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLiquibaseApplication.class, args);
+	}
+
+}
+
