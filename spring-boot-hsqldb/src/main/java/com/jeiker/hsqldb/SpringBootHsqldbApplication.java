710ad207ed264a224c5261c6daec9beb757aba17
jeikerxiao <jeiker@126.com>
2018-12-18 11:13:30 +0800

    add spring-boot-hsqldb
    add spring-boot-hsqldb

---
+++ spring-boot-hsqldb/src/main/java/com/jeiker/hsqldb/SpringBootHsqldbApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.hsqldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHsqldbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHsqldbApplication.class, args);
+	}
+
+}
+
