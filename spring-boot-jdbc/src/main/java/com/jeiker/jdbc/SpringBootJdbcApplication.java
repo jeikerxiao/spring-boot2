f15336e0fe0a9b1a8554c4c28eb1953f2d387173
jeikerxiao <jeiker@126.com>
2018-12-18 11:07:22 +0800

    add spring-boot-jdbc
    add spring-boot-jdbc

---
+++ spring-boot-jdbc/src/main/java/com/jeiker/jdbc/SpringBootJdbcApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcApplication.class, args);
+	}
+
+}
+
