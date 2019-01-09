de3df9611396535d65494b88edc1ccaf16df6e7e
jeikerxiao <jeiker@126.com>
2019-01-09 11:31:46 +0800

    add: spring-boot-weixin project.

---
+++ spring-boot-weixin/README.md
@@ -0,0 +1,13 @@
+# spring-boot-weixin
+
+Spring Boot 配合 WxJava 开发微信公众号。
+
+* Spring Boot: 2.1.1
+* weixin-java-mp: 3.3.0
+
+# 说明
+
+* 微信开发文档： https://mp.weixin.qq.com/wiki
+* WxJava地址：https://github.com/Wechat-Group/WxJava
+
+
---
+++ spring-boot-weixin/src/main/java/com/jeiker/weixin/SpringBootWeixinApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeixinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWeixinApplication.class, args);
+    }
+
+}
+
