79bf1845046d66dc2ac2ebaf2dda392c4b0faa61
jeikerxiao <jeiker@126.com>
2019-01-09 13:41:46 +0800

    add: unit test.

---
+++ spring-boot-weixin/src/test/java/com/jeiker/weixin/config/WeChatAccountConfigTest.java
@@ -0,0 +1,31 @@
+package com.jeiker.weixin.config;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Description: 测试配置参数读取
+ * User: jeikerxiao
+ * Date: 2019/1/9 1:38 PM
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeChatAccountConfigTest {
+
+    @Autowired
+    private WeChatAccountConfig weChatAccountConfig;
+
+    @Test
+    public void getMpAppId() {
+        Assert.assertNotNull(weChatAccountConfig.getMpAppId());
+    }
+
+    @Test
+    public void getMpAppSecret() {
+        Assert.assertNotNull(weChatAccountConfig.getMpAppSecret());
+    }
+}
