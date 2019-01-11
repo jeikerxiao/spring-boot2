05c28ba5ae3a6a7fdf8949290ce264bbbdbcaaa3
jeikerxiao <jeiker@126.com>
2019-01-11 17:00:06 +0800

    add: user service

--- spring-boot-cache/README.md
+++ spring-boot-cache/README.md
@@ -2,6 +2,11 @@
 
 Spring Boot 整合 cache。
 
+
+Spring Cache 对 Cahce 进行了抽象，提供了 @Cacheable、@CachePut、@CacheEvict 等注解。
+
+Spring Boot 应用基于 Spring Cache，既提供了基于内存实现的缓存管理器，可以用于单体应用系统，也集成了 EhCache、Redis 等缓存服务器，可以用于大型系统或者分布式系统。
+
 # 说明
 
 
---
+++ spring-boot-cache/src/main/java/com/jeiker/cache/dao/UserRepository.java
@@ -0,0 +1,16 @@
+package com.jeiker.cache.dao;
+
+import com.jeiker.cache.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Description: UserRepository 接口操作User对象
+ * User: jeikerxiao
+ * Date: 2019/1/9 2:26 PM
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByName(String name);
+}
