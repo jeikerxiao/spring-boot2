d9bbdb6ad48a32be70232a3cc61ad8ddc8398239
jeikerxiao <jeiker@126.com>
2019-01-17 19:42:24 +0800

    add: excel demo

---
+++ spring-boot-excel/src/main/java/com/jeiker/excel/SpringBootExcelApplication.java
@@ -0,0 +1,14 @@
+package com.jeiker.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExcelApplication.class, args);
+    }
+
+}
+
