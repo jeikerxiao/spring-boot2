4d185eccfc80689f092e76f098ea1f7d03b188d8
jeikerxiao <jeiker@126.com>
2019-05-27 17:06:19 +0800

    add: zookeeper project

---
+++ spring-boot-zookeeper/README.md
@@ -0,0 +1,5 @@
+# spring-boot-zookeeper
+
+Spring Boot 配合 zookeeper。
+
+
---
+++ spring-boot-zookeeper/src/main/java/com/jeiker/zookeeper/SpringBootZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.jeiker.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootZookeeperApplication.class, args);
+    }
+
+}
