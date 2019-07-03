3bf68d33c1a5fba1638b2a51be6e19a0d516e0b7
jeikerxiao <jeiker@126.com>
2019-07-03 14:36:38 +0800

    add swagger config

---
+++ spring-boot-swagger/src/main/java/com/jeiker/swagger/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.jeiker.swagger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Description: spring-boot-swagger
+ * User: jeikerxiao
+ * Date: 2019/7/3 11:10 AM
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.jeiker.swagger.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("springboot利用swagger构建api文档")
+                .description("简单优雅的restfun风格，http://csxbank.com")
+                .termsOfServiceUrl("http://csxbank.com")
+                .version("1.0")
+                .build();
+    }
+}
