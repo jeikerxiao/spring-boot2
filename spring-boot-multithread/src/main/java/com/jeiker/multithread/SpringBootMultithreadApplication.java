4c01ddac6bcb71c606d19af56c35ff107ff54cdd
jeikerxiao <jeiker@126.com>
2019-08-20 14:32:20 +0800

    init multi-thread project

---
+++ spring-boot-multithread/src/main/java/com/jeiker/multithread/SpringBootMultithreadApplication.java
@@ -0,0 +1,13 @@
+package com.jeiker.multithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultithreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultithreadApplication.class, args);
+    }
+
+}
